48e675208ba446f67f099ae04367c9fa110872a9
1Hendrik2 <hendrik.metsallik@gmail.com>
2026-06-16 02:38:58 +0800

    MP-6|next config update

--- next.config.mjs
+++ next.config.mjs
@@ -2,4 +2,6 @@
 const nextConfig = {};
 export default nextConfig;
 
-import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
+// OpenNext Cloudflare dev helper - commented out because @opennextjs/cloudflare is not installed.
+// To use the modern OpenNext adapter, install the package and uncomment this line.
+// import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
