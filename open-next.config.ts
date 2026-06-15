4397853b87ca248346b2a4714208ae6a2a0f9f2c
1Hendrik2 <hendrik.metsallik@gmail.com>
2026-06-16 02:25:29 +0800

    MP-5|Replacing opennext with nextonpages

--- open-next.config.ts
+++
@@ -1,7 +0,0 @@
-// open-next.config.ts
-import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-
-export default defineCloudflareConfig({
-	// For a simple portfolio, the default config is enough.
-	// You can enable R2 caching later if needed.
-});
